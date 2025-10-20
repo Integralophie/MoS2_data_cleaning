23b127c80b5730da4d72443c19f402ca347c1bc4
Sophie Wu <42869334+Integralophie@users.noreply.github.com>
2025-10-20 12:05:37 -0500

    Finished all python notebooks for IdVd, IdVtg, diffBG

--- MoS2_data_cleaning.py
+++ MoS2_data_cleaning.py
@@ -2,6 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+
+'''
+Starting IdVd
+
+'''
 def read_csv_direction(filename):
     '''
     Reads the csv file, converts `ID` unit to uA, and adds a line representing
@@ -71,7 +76,10 @@ def reorder_by_VD(df_sorted):
 
 # sorted_df.to_csv('output.csv', index=True)
 
+'''
+Starting IdVtg
 
+'''
 def read_csv_IdVtg(filename):
     df  = pd.read_csv(filename, skiprows=256)
     df.drop(df.columns[0], axis=1, inplace=True)
@@ -88,10 +96,14 @@ def read_csv_IdVtg(filename):
 
 
 
-def reshape_table_by_VG(df):
+def reshape_table_by_VD(df):
     df_new = pd.pivot_table(df,columns=' VD',values=[' ID',' ITG',' Ibg'],index=[' VTG',' forward'])
     df_sorted = df_new.sort_values(by=[' forward',' VTG'])
     return df_sorted
+
+
+
+
 
 
 def reorder_by_VG(df_sorted, Vbg):
@@ -127,5 +139,137 @@ def reorder_by_VG(df_sorted, Vbg):
     df_new['Id, V_D = 0.1'] = abs(df_new['Id, V_D = 0.1'])
     df_new.drop(columns=[' forward', 'Itg, V_D = 0.4', 'Itg, V_D = 0.7', 'Itg, V_D = 1.0','Ibg, V_D = 0.4', 'Ibg, V_D = 0.7', 'Ibg, V_D = 1.0'],inplace=True)
     df_new[' Vbg'] = Vbg
+    df_abs = df_new.abs()
 
-    return df_new
+    return df_abs
+
+
+'''
+Start here, IdVtg with different back gate
+
+
+'''
+def read_csv_IdVtg_diffBG(filename):
+    df  = pd.read_csv(filename, skiprows=256)
+    df.drop(df.columns[0], axis=1, inplace=True)
+
+    Vbg = df.iloc[1, 2]
+    df.drop(columns=[' VD', ' IS'], inplace=True)
+
+    df[' forward'] = True
+
+    df.loc[(df.index >= 201) & (df.index <= 401), ' forward'] = False
+    df.loc[(df.index >= 603) & (df.index <= 803), ' forward'] = False
+    df.loc[(df.index >= 1005) & (df.index <= 1205), ' forward'] = False
+    df.loc[(df.index >= 1407) & (df.index <= 1607), ' forward'] = False
+    df.loc[(df.index >= 1809) & (df.index <= 2007), ' forward'] = False
+    return Vbg, df
+
+
+
+
+def reshape_table_by_VG_diffBG(df):
+    df_new = pd.pivot_table(df,columns=' Vbg',values=[' ID'],index=[' VTG',' forward'])
+    df_sorted = df_new.sort_values(by=[' forward',' VTG'])
+    return df_sorted
+
+
+def reorder_by_VG_diffBG(df_sorted, Vbg):
+    midpoint = len(df_sorted) // 2
+
+    # Split the DataFrame into two halves
+    first_half = df_sorted.iloc[:midpoint]
+    second_half = df_sorted.iloc[midpoint:]
+
+    # Sort the first half in ascending order based on 'col1'
+    first_half_sorted = first_half.sort_values(by=' VTG', ascending=True)
+
+    # Sort the second half in descending order based on 'col1'
+    second_half_sorted = second_half.sort_values(by=' VTG', ascending=False)
+
+    # Concatenate the sorted halves back together
+    sorted_df = pd.concat([first_half_sorted, second_half_sorted])
+
+    column_names_list = sorted_df.columns.tolist()
+    new_list = []
+    for tup in column_names_list:
+        new_list.append(f'Id, V_bg = {tup[1]}')
+    sorted_df.columns = new_list
+    df_new = sorted_df.reset_index().set_index(' VTG')
+    df_new.drop(columns=[' forward'],inplace=True)
+
+    df_abs = df_new.abs()
+
+    return df_abs
+
+
+
+'''
+
+IdVtg with double gate 
+
+
+'''
+
+def read_csv_IdVtg_double(filename):
+    df  = pd.read_csv(filename, skiprows=256)
+    df.drop(df.columns[0], axis=1, inplace=True)
+    Vbg = df.iloc[1, 2]
+    df.drop(columns=[' Vbg', ' IS'], inplace=True)
+
+    df[' forward'] = True
+
+    df.loc[(df.index >= 201) & (df.index <= 401), ' forward'] = False
+    df.loc[(df.index >= 603) & (df.index <= 803), ' forward'] = False
+    df.loc[(df.index >= 1005) & (df.index <= 1205), ' forward'] = False
+    df.loc[(df.index >= 1407) & (df.index <= 1607), ' forward'] = False
+    return Vbg, df
+
+
+
+def reshape_table_by_VD_double(df):
+    df_new = pd.pivot_table(df,columns=' VD',values=[' ID',' ITG'],index=[' VTG',' forward'])
+    df_sorted = df_new.sort_values(by=[' forward',' VTG'])
+    return df_sorted
+
+
+
+
+
+
+def reorder_by_VG_double(df_sorted, Vbg):
+    midpoint = len(df_sorted) // 2
+
+    # Split the DataFrame into two halves
+    first_half = df_sorted.iloc[:midpoint]
+    second_half = df_sorted.iloc[midpoint:]
+
+    # Sort the first half in ascending order based on 'col1'
+    first_half_sorted = first_half.sort_values(by=' VTG', ascending=True)
+
+    # Sort the second half in descending order based on 'col1'
+    second_half_sorted = second_half.sort_values(by=' VTG', ascending=False)
+
+    # Concatenate the sorted halves back together
+    sorted_df = pd.concat([first_half_sorted, second_half_sorted])
+
+    column_names_list = sorted_df.columns.tolist()
+
+    new_list = []
+    for tup in column_names_list[0:4]:
+        new_list.append(f'Id, V_D = {tup[1]}')
+    for tup in column_names_list[4:8]:
+        new_list.append(f'Itg, V_D = {tup[1]}')
+
+
+    sorted_df.columns = new_list
+
+    df_new = sorted_df.reset_index().set_index(' VTG')
+    df_new['Itg, V_D = 0.1'] = abs(df_new['Itg, V_D = 0.1'])
+    df_new['Id, V_D = 0.1'] = abs(df_new['Id, V_D = 0.1'])
+    df_new.drop(columns=[' forward', 'Itg, V_D = 0.4', 'Itg, V_D = 0.7', 'Itg, V_D = 1.0'],inplace=True)
+
+    df_abs = df_new.abs()
+
+    return df_abs
+
